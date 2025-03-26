9624f545cc5f4d1cd493633c54ce00bc69f18612
alexey <suvorov@premiumbonus.ru>
2025-03-26 17:54:17 +0300

    начальный коммит, установка зависимостей и настройка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Nora from '@primeng/themes/nora';
+import Laura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Laura,
+        options: {
+          prefix: 'p',
+          // сломал своим стилем (которого нет), чтобы убралась темная тема
+          darkModeSelector: 'my-style',
+          cssLayer: false
+        },
+      }
+    })
+  ]
 };
